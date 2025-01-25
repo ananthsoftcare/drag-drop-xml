6c43dcb7d34626abe4f6d3f650c7fcc3d110eef0
ananth <ananthsoftcare@gmail.com>
2025-01-26 01:38:43 +0530

    removed FE

--- app/src/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import testReducer from './testReducer';
-const firstTestApp = (currentState = {}, action) => {
-    return {
-        testReducer: testReducer(currentState.testReducer, action)
-    }
-};
-
-export default firstTestApp;
